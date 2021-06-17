bd46ddf1a526c5d16db1a4bd0236f49893e6b20a
Jeff Chiu <jwchiu82@gmail.com>
2021-06-16 21:04:03 -0700

    get working calculator

--- calculator.py
+++ calculator.py
@@ -12,12 +12,33 @@ def subtract(n1,n2):
   """subtracts first and second number"""
   return n1 - n2
 
-
+#multiply
 def multiply(n1,n2):
   """multiplies first and second number"""
   return n1 * n2
 
-
+#divides
 def divide(n1,n2):
   """divides first and second number"""
   return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+}
+
+num1 = int(input("What's the first number?: "))
+
+for sym in operations:
+  print(sym)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
+num2 = int(input("What's the second number?: ")) 
+
+calculations_function = operations[operation_symbol]
+answer = calculations_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
