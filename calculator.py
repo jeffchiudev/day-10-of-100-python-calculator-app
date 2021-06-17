f2f302b6188153dd49001d9188d2b477dbaac6c9
Jeff Chiu <jwchiu82@gmail.com>
2021-06-16 20:51:01 -0700

    set up initial calculator functions

---
+++ calculator.py
@@ -0,0 +1,23 @@
+from art import logo
+
+print(logo)
+
+#Add
+def add(n1,n2):
+  """Adds first and second number"""
+  return n1 + n2
+
+#subtract
+def subtract(n1,n2):
+  """subtracts first and second number"""
+  return n1 - n2
+
+
+def multiply(n1,n2):
+  """multiplies first and second number"""
+  return n1 * n2
+
+
+def divide(n1,n2):
+  """divides first and second number"""
+  return n1 / n2
